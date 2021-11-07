6dcfdf8cb149382cd71b69942c0a38773e71f6ac
Ernesto Tagwerker <ernesto+github@ombulabs.com>
2021-11-06 21:42:17 -0400

    Add new table to support projects for every report

---
+++ db/migrate/20211106175125_add_projects.rb
@@ -0,0 +1,18 @@
+class AddProjects < ActiveRecord::Migration[6.1]
+  def up
+    create_table :projects do |t|
+      t.string :git_url
+      t.string :permalink
+      t.string :name
+      t.timestamps
+    end
+
+    add_reference :reports, :project, foreign_key: true
+  end
+
+  def down
+    remove_reference :reports, :project, foreign_key: true
+
+    drop_table :projects
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -2,15 +2,15 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_27_014813) do
+ActiveRecord::Schema.define(version: 2021_11_06_175125) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,12 +28,23 @@ ActiveRecord::Schema.define(version: 2021_03_27_014813) do
     t.index ["report_id"], name: "index_analyzed_files_on_report_id"
   end
 
+  create_table "projects", force: :cascade do |t|
+    t.string "git_url"
+    t.string "permalink"
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "reports", id: :serial, force: :cascade do |t|
     t.text "report"
     t.boolean "compare"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "slug"
+    t.bigint "project_id"
+    t.index ["project_id"], name: "index_reports_on_project_id"
   end
 
+  add_foreign_key "reports", "projects"
 end
